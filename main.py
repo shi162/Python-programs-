b764a0298d6c44f19a555180ed72bd467277ba2e
shi162 <fatimaakthershimu@gmail.com>
2026-06-26 17:18:17 +0600

    variable

---
+++ main.py
@@ -0,0 +1,23 @@
+                  # primt
+
+print ("Assalamualikum")
+print ("my name FA Shi")
+print ("I am a Python coder")
+print ("i am from Bangladesh")
+print ("I already know about enough base form of Python but I am as a new github user try to start again Python with beggin ")
+
+
+               #variable and Datatype
+
+name = "FA Shi"
+country = "Bangladesh"
+Department = "CST"
+favourit_language = "Python"
+
+print (name)
+print (country)
+print (Department)
+print (favourit_language)
+
+
+
